0a143f44ca32863747ce14d723538c076a4bb762
kumagai <kumagai@toshiba-tops.co.jp>
2017-07-18 13:48:44 +0900

    highlight()関数化・文字列長チェック追加

--- html/listday.js
+++ html/listday.js
@@ -1,36 +1,40 @@
 
 /**
+ * 指定のHTML文字列の<span>タグによるハイライト加工
+ */
+function highlight(text, keyword)
+{
+	if (text.indexOf('<span class=\"highlight\">') >= 0 &&
+		text.indexOf('</span>') >= 0)
+	{
+		text = text.replace(new RegExp('<span class=\"highlight\">', 'g'), '');
+		text = text.replace(new RegExp('</span>', 'g'), '');
+	}
+
+	if ((keyword.length > 0) && (text.indexOf(keyword) >= 0))
+	{
+		text = text.replace(new RegExp(keyword, 'g'), '<span class=\"highlight\">' + keyword + '</span>');
+	}
+
+	return text;
+}
+
+/**
  * 指定のキーワードで検索する
  */
 function searchKeyword(keywordControl)
 {
 	var i;
-	var update;
-	var text;
+	var text, text2;
 	var table = document.getElementById('sorter');
 	var keyword = keywordControl.value;
 
 	for (i=1 ; i < table.rows.length ; i++)
 	{
-		update = false;
 		text = table.rows[i].cells[2].innerHTML;
+		text2 = highlight(text, keyword);
 
-		if (text.indexOf('<span class=\"highlight\">') >= 0 &&
-			text.indexOf('</span>') >= 0)
-		{
-			text = text.replace(new RegExp('<span class=\"highlight\">', 'g'), '');
-			text = text.replace(new RegExp('</span>', 'g'), '');
-			update = true;
-		}
-
-		if (text.indexOf(keyword) >= 0)
-		{
-			text = text.replace(new RegExp(keyword, 'g'), '<span class=\"highlight\">' + keyword + '</span>');
-			update = true;
-		}
-		console.log(update + ' ' + table.rows[i].cells[2].innerHTML);
-
-		if (update)
+		if (text != text2)
 		{
 			table.rows[i].cells[2].innerHTML = text;
 		}
