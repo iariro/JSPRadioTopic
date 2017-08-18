5c02c37d9d1c6ea6b13e27412b38c2d2435eb2b3
kumagai <kumagai@toshiba-tops.co.jp>
2017-08-18 14:57:46 +0900

    全件１DBアクセスで取得して日付チェックを高速化

--- src/kumagai/radiotopic/struts2/ErrorCheckAction.java
+++ src/kumagai/radiotopic/struts2/ErrorCheckAction.java
@@ -41,46 +41,7 @@ public class ErrorCheckAction
 			try
 			{
 				Connection connection = DriverManager.getConnection(url);
-				ProgramCollection programCollection =
-					new ProgramCollection(connection);
-				for (Program program : programCollection)
-				{
-					DayCollection days =
-						new DayCollection
-							(connection, program.id, SortOrder.NumberByNumeric);
-					Day pday = null;
-					for (Day day : days)
-					{
-						if (pday != null)
-						{
-							// 比較する回あり
-
-							if (day != null && day.date != null && pday != null && pday.date != null)
-							{
-								// 日情報はそろっている
-
-								if (day.date.compareTo(pday.date) > 0)
-								{
-									// 新しいはずの日の方が古い
-
-									invalidDates.add(
-										String.format(
-											"%sの%s:%sと%s:%sの前後関係が異常",
-											program.name,
-											day.getNo(),
-											day.getDate(),
-											pday.getNo(),
-											pday.getDate()));
-								}
-							}
-						}
-
-						if (day.date != null)
-						{
-							pday = day;
-						}
-					}
-				}
+				invalidDates = checkDateFast(connection);
 				return "success";
 			}
 			catch (SQLException exception)
@@ -100,4 +61,115 @@ public class ErrorCheckAction
 			return "error";
 		}
 	}
+
+	/**
+	 * 日付チェック。番組ごとDBアクセスし遅い。
+	 * @param connection DB接続オブジェクト
+	 * @return チェック結果文字列
+	 */
+	protected ArrayList<String>  checkDateNormal(Connection connection)
+		throws SQLException
+	{
+		ArrayList<String> invalidDates = new ArrayList<String>();
+		ProgramCollection programCollection =
+			new ProgramCollection(connection);
+		for (Program program : programCollection)
+		{
+			DayCollection days =
+				new DayCollection
+					(connection, program.id, SortOrder.NumberByNumeric);
+			Day pday = null;
+			for (Day day : days)
+			{
+				if (pday != null)
+				{
+					// 比較する回あり
+
+					if (day != null && day.date != null && pday != null && pday.date != null)
+					{
+						// 日情報はそろっている
+
+						if (day.date.compareTo(pday.date) > 0)
+						{
+							// 新しいはずの日の方が古い
+
+							invalidDates.add(
+								String.format(
+									"%sの%s:%sと%s:%sの前後関係が異常",
+									program.name,
+									day.getNo(),
+									day.getDate(),
+									pday.getNo(),
+									pday.getDate()));
+						}
+					}
+				}
+
+				if (day.date != null)
+				{
+					pday = day;
+				}
+			}
+		}
+
+		return invalidDates;
+	}
+
+	/**
+	 * 日付チェック。全件１DBアクセスで取得し速い。
+	 * @param connection DB接続オブジェクト
+	 * @return チェック結果文字列
+	 */
+	public ArrayList<String> checkDateFast(Connection connection)
+		throws SQLException
+	{
+		String sql = "select program.name, day.id, programid, date, no, createdate, updatedate from day join program on program.id=day.programid where program.id=? order by programid, no desc";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		ResultSet results = statement.executeQuery();
+
+		Day pday = null;
+		while (results.next())
+		{
+			Day day = new Day(results);
+			if (pday != null)
+			{
+				// 比較する回あり
+
+				if (day.programid != pday.programid)
+				{
+					// 違う番組
+
+					pday = null;
+				}
+
+				if (day != null && day.date != null && pday != null && pday.date != null)
+				{
+					// 日情報はそろっている
+
+					if (day.date.compareTo(pday.date) > 0)
+					{
+						// 新しいはずの日の方が古い
+
+						invalidDates.add(
+							String.format(
+								"%sの%s:%sと%s:%sの前後関係が異常",
+								day.programName,
+								day.getNo(),
+								day.getDate(),
+								pday.getNo(),
+								pday.getDate()));
+					}
+				}
+			}
+			pday = day;
+		}
+
+		results.close();
+		statement.close();
+
+		return null;
+	}
+
 }
